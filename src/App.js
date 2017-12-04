0ebc20f6f1b6a15f1343af5e4b7d849046945d57
kwj <wwjwangweijia@163.com>
2017-12-04 21:02:56 +0800

    test

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
       <p className="App-intro">
         To get started, edit
         <code>src/App.js</code>
-        and save to reload.
+        and save to reload.wwj
       </p>
     </div>);
   }
