c155249a727e511b09bcd82cc82315f0dacced81
kwj <wwjwangweijia@163.com>
2017-12-10 14:20:22 +0800

    创建介绍页面

--- src/project/src/ComponentRoutes.js
+++ src/project/src/ComponentRoutes.js
@@ -1,14 +1,20 @@
-import Home from '../notes/home/Home.js';
-
-export const myRoutes = [
-    {
-        name: "MyNotes",
-        path: "/myNotes",
-        component: Home,
-        level: 0,
-        exact: true,
-        haveChildern: false,
-        childern: [
-        ]
-    }
+import Home from '../notes/home/Home';
+import Recommend from '../notes/home/recommend/Recommend';
+export const myRoutes = [{
+name:"MyNotes",
+path:"/home",
+component:Home,
+level:0,
+exact:true,
+haveChildern:true,
+childern:[{
+name:"介绍",
+path:"/home/recommend",
+component:Recommend,
+level:1,
+exact:true,
+haveChildern:false,
+childern:[]
+},
 ]
+},]
