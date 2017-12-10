cbd961c31d8c00a8f49052908a1d65d3fbb9d67a
kwj <wwjwangweijia@163.com>
2017-12-10 14:04:53 +0800

    删除测试页面

--- src/project/src/ComponentRoutes.js
+++ src/project/src/ComponentRoutes.js
@@ -1,68 +1,14 @@
 import Test1 from '../notes/test1/Test1';
-import WwjXx from '../notes/test1/xx/WwjXx';
-import Test2 from '../notes/test2/Test2';
-import Test2_1 from '../notes/test2/test2_1/Test2_1';
-import Test6_1 from '../notes/test2/test2_1/test6_1/Test6_1';
-import Test3 from '../notes/test3/Test3';
-import Test3_1 from '../notes/test3/test3_1/Test3_1';
-export const myRoutes = [{
-name:"test1",
-path:"/test1",
-component:Test1,
-level:0,
-exact:true,
-haveChildern:true,
-childern:[{
-name:"wwjlovexx",
-path:"/test1/xx",
-component:WwjXx,
-level:1,
-exact:true,
-haveChildern:false,
-childern:[]
-},
+
+export const myRoutes = [
+    {
+        name: "test1",
+        path: "/test1",
+        component: Test1,
+        level: 0,
+        exact: true,
+        haveChildern: false,
+        childern: [
+        ]
+    }
 ]
-},{
-name:"test2",
-path:"/test2",
-component:Test2,
-level:0,
-exact:true,
-haveChildern:true,
-childern:[{
-name:"test2_1",
-path:"/test2/test2_1",
-component:Test2_1,
-level:1,
-exact:true,
-haveChildern:true,
-childern:[{
-name:"test6_1",
-path:"/test2/test2_1/test6_1",
-component:Test6_1,
-level:2,
-exact:true,
-haveChildern:false,
-childern:[]
-},
-]
-},
-]
-},{
-name:"test3",
-path:"/test3",
-component:Test3,
-level:0,
-exact:true,
-haveChildern:true,
-childern:[{
-name:"test3_1",
-path:"/test3/test3_1",
-component:Test3_1,
-level:1,
-exact:true,
-haveChildern:false,
-childern:[]
-},
-]
-},]
