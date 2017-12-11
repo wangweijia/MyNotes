62441facb89e40cd999011b3709afec6f001223d
kwj <wwjwangweijia@163.com>
2017-12-11 22:15:36 +0800

    创建问题

---
+++ src/project/notes/interview/1513001629/Com1513001629.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Com1513001629 extends Component {
+    render() {
+        return (
+            <div style={{display: 'flex', flex: 1}}>
+                ios loadview viewdidload:undefined
+            </div>
+        );
+    }
+}
--- src/project/src/ComponentRoutes.js
+++ src/project/src/ComponentRoutes.js
@@ -1,4 +1,5 @@
 import Recommend from '../notes/recommend/Recommend';
+import Com1513001629 from '../notes/interview/1513001629/Com1513001629';
 import Interview from '../notes/interview/Interview';
 export const myRoutes = [{
 name:"介绍",
@@ -14,6 +15,15 @@ path:"/interview",
 component:Interview,
 level:0,
 exact:true,
+haveChildern:true,
+childern:[{
+name:"ios loadview viewdidload",
+path:"/interview/1513001629",
+component:Com1513001629,
+level:1,
+exact:true,
 haveChildern:false,
 childern:[]
+},
+]
 },]
