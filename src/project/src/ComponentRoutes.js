23779ffbf9753fcaa57baff694c55b612670627d
kwj <wwjwangweijia@163.com>
2017-12-11 21:45:12 +0800

    创建面试题汇总页面

--- src/project/src/ComponentRoutes.js
+++ src/project/src/ComponentRoutes.js
@@ -1,8 +1,17 @@
 import Recommend from '../notes/recommend/Recommend';
+import Interview from '../notes/interview/Interview';
 export const myRoutes = [{
 name:"介绍",
 path:"/recommend",
 component:Recommend,
+level:0,
+exact:true,
+haveChildern:false,
+childern:[]
+},{
+name:"面试问题汇总",
+path:"/interview",
+component:Interview,
 level:0,
 exact:true,
 haveChildern:false,
