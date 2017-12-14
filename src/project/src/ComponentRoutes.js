b901af7c2d059a75e5c6634b1feb9ed1f63f723c
kwj <wwjwangweijia@163.com>
2017-12-14 12:49:15 +0800

    清空项目

--- src/project/src/ComponentRoutes.js
+++ src/project/src/ComponentRoutes.js
@@ -1,10 +1,1 @@
-import Recommend from '../notes/recommend/Recommend';
-export const myRoutes = [{
-name:"介绍",
-path:"/recommend",
-component:Recommend,
-level:0,
-exact:true,
-haveChildern:false,
-childern:[]
-},]
+export const myRoutes = []
