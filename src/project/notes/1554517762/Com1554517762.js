be41d8b4d54ea214f406eebdd90f5d199a6f570f
weijia <wangweijia@weijias-MBP.lan>
2019-04-06 10:31:25 +0800

    add read me index

---
+++ src/project/notes/1554517762/Com1554517762.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Com1554517762 extends Component {
+    render() {
+        return (
+            <div style={{display: 'flex', flex: 1}}>
+                npm run tools
+            </div>
+        );
+    }
+}
--- src/project/src/ComponentRoutes.js
+++ src/project/src/ComponentRoutes.js
@@ -1,1 +1,10 @@
-export const myRoutes = []
+import Com1554517762 from '../notes/1554517762/Com1554517762';
+export const myRoutes = [{
+name:"ReadMe",
+path:"/1554517762",
+component:Com1554517762,
+level:0,
+exact:true,
+haveChildern:false,
+childern:[]
+},]
